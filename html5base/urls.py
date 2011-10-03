2ab6e816c9d56d8f51a94ce28e466b28890dad90
Vladimir Tartynskyi <fon.vosi@gmail.com>
2011-10-03 18:17:21 +0300

    * tools

--- html5base/urls.py
+++ html5base/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('django.views.generic.simple',
-    (r'crossdomain.xml', 'direct_to_template', {'template': 'crossdomain.xml'}),
-    (r'robots.txt', 'direct_to_template', {'template': 'robots.txt'}),
-    (r'PIE.htc', 'direct_to_template', {'template': 'PIE.htc'}),
+    (r'crossdomain.xml', 'direct_to_template', {'template': 'html5base/crossdomain.xml'}),
+    (r'robots.txt', 'direct_to_template', {'template': 'html5base/robots.txt'}),
+    (r'PIE.htc', 'direct_to_template', {'template': 'html5base/PIE.htc'}),
 )
