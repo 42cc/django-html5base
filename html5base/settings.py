12723284a67ba5c023ec5fd9981f101443de850f
Vladimir Tartynskyi <fon.vosi@gmail.com>
2011-10-03 20:13:13 +0300

    * tools

--- html5base/settings.py
+++ html5base/settings.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 
 DEFAULT_SETTINGS = {
+    'yandex_vrfy': '',
+    'yandex_metrika_id': '',
+    'google_vrfy': '2',
+    'google_analytics_id': '',
+    'google_analytics_site': '',
 }
 
 USER_SETTINGS = getattr(settings, 'HTML5BASE_SETTINGS')
 DEFAULT_SETTINGS.update(USER_SETTINGS)
-
-
-globals().update(DEFAULT_SETTINGS)
--- html5base/templatetags/html5base_tags.py
+++ html5base/templatetags/html5base_tags.py
@@ -1,23 +1,26 @@
 from django import template
-from django.conf import settings
+from html5base import settings
 import re
 
 register = template.Library()
 
-"""
-@register.inclusion_tag('includes/google_analytics.html', takes_context=True)
-def google_analytics(context, ga_tracking_code):
-	pass
+@register.inclusion_tag('html5base/includes/google_analytics.html', takes_context=True)
+def google_analytics(context):
+    context.update({'id': settings.DEFAULT_SETTINGS.get('google_analytics_id'),\
+                    'site': settings.DEFAULT_SETTINGS.get('google_analytics_site')})
+    return context
 
-@register.inclusion_tag('includes/google_webmaster.html', takes_context=True)
-def google_webmaster(context, ga_webmaster_code):
-	pass
+@register.inclusion_tag('html5base/includes/google_webmaster.html', takes_context=True)
+def google_webmaster(context):
+    context.update({'id': settings.DEFAULT_SETTINGS.get('google_vrfy')})
+    return context
 
-@register.inclusion_tag('includes/yandex_metrics.html', takes_context=True)
-def yandex_metrics(context, ya_tracking_code):
-	pass
+@register.inclusion_tag('html5base/includes/yandex_metrics.html', takes_context=True)
+def yandex_metrics(context):
+    context.update({'id': settings.DEFAULT_SETTINGS.get('yandex_metrika_id')})
+    return context
 
-@register.inclusion_tag('includes/yandex_webmaster.html', takes_context=True)
-def yandex_webmaster(context, ya_webmaster_code):
-	pass
-"""
+@register.inclusion_tag('html5base/includes/yandex_webmaster.html', takes_context=True)
+def yandex_webmaster(context):
+    context.update({'id': settings.DEFAULT_SETTINGS.get('yandex_vrfy')})
+    return context
