317e9e98474040c0c2fbee0d73d8dc3b686bbaed
Vladimir Tartynskyi <fon.vosi@gmail.com>
2011-12-23 16:38:58 +0200

    html update, jquery update, +alexa

--- html5base/settings.py
+++ html5base/settings.py
@@ -3,9 +3,10 @@ from django.conf import settings
 DEFAULT_SETTINGS = {
     'yandex_vrfy': '',
     'yandex_metrika_id': '',
-    'google_vrfy': '2',
+    'google_vrfy': '',
     'google_analytics_id': '',
     'google_analytics_site': '',
+    'alexa_vrfy': '',
 }
 
 USER_SETTINGS = getattr(settings, 'HTML5BASE_SETTINGS')
